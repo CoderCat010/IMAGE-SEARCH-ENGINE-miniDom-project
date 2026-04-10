0003da40f8868705ea16561da84ece97ae90fcf7
CoderCat010 <nusraumme@gmail.com>
2026-04-11 05:57:33 +0600

    added all id

---
+++ script.js
@@ -0,0 +1,11 @@
+// get search box filed to get search anything 
+const inputBox = document.getElementById('search-box');
+
+// get search button
+const searchBtn = document.getElementById('search-btn');
+
+// img container that shows anything on the page
+const imgConatiner = document.getElementById('img-container');
+
+// show more result button
+const showMoreBtn = document.getElementById('showMore-btn');
